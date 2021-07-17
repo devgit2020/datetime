645e27588bd5c6489983138b989a90f32756ddc3
devuser <santosh.behera@capgemini.com>
2021-07-17 12:22:00 +0530

    new changes

--- src/StreamTst.java
+++ src/StreamTst.java
@@ -9,12 +9,13 @@ public class StreamTst {
 		// TODO Auto-generated method stub
 		
 		 List<Product> productsList = new ArrayList<Product>();  
-	        //Adding Products  
+	        //Adding Products 
 	        productsList.add(new Product(1,"HP Laptop",25000f));  
 	        productsList.add(new Product(2,"Dell Laptop",30000f));  
 	        productsList.add(new Product(3,"Lenevo Laptop",28000f));  
 	        productsList.add(new Product(4,"Sony Laptop",28000f));  
 	        productsList.add(new Product(5,"Apple Laptop",90000f));  
+	        
 	        
 	       Product max = productsList.stream().max((product1, product2) -> product1.price > product2.price ? 1 : -1).get();
 	       
